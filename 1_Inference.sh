9e3d5f7dd7fa83f32163d5ea3cac6a88dd1a7e64
Monica Romero <74182452+monirome@users.noreply.github.com>
2023-12-18 22:39:42 +0100

    Add files via upload

---
+++ 1_Inference.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define the base directory for data and model
+DATA_DIR="./data"
+MODEL_NAME="{language}_model.pt"
+RESULTS_DIR="./transcriptions"
+
+# Docker command to run inference
+docker run -it --network=host --name "fairseq_inference" --shm-size=8g --ulimit memlock=-1 \
+-v $PWD/$DATA_DIR:/data/ -w /workspace/fairseq fairseq \
+python3 infer.py \
+/data/ \
+--gen-subset "test" \
+--path "/data/$MODEL_NAME" \
+--results-path "/data/$RESULTS_DIR/" \
+--w2l-decoder viterbi \
+--scoring wer \
+--batch-size 1 \
+--num-workers 8 \
+--task audio_finetuning \
+--nbest 1 \
+--post-process letter \
+--criterion ctc \
+--labels ltr \
+--max-sample-size 300000000 \
+--min-sample-size 16000 \
+--eval-wer \
+--cpu
+
